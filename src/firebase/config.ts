6a1ac6fa96e89b7cf84b5d99a319a24389d1df4b
dongnv11082001 <hiiamgosu4@gmail.com>
2023-09-21 01:22:14 +0700

    update .gitignore, eslint rules

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDbBaeTO05-rM82SdK07fQP4x9Kd5uvgk4',
-  authDomain: 'muso-ninjas-610cd.firebaseapp.com',
-  projectId: 'muso-ninjas-610cd',
-  storageBucket: 'muso-ninjas-610cd.appspot.com',
-  messagingSenderId: '11324743213',
-  appId: '1:11324743213:web:55c3da7a3f73b8c0f122a4',
-  measurementId: 'G-JXT7CYNENH',
+  apiKey: "AIzaSyDbBaeTO05-rM82SdK07fQP4x9Kd5uvgk4",
+  authDomain: "muso-ninjas-610cd.firebaseapp.com",
+  projectId: "muso-ninjas-610cd",
+  storageBucket: "muso-ninjas-610cd.appspot.com",
+  messagingSenderId: "11324743213",
+  appId: "1:11324743213:web:55c3da7a3f73b8c0f122a4",
+  measurementId: "G-JXT7CYNENH",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
